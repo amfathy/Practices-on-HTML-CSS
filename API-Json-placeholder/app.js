b5c9fa97302de46808fd35c2f9310ef22af687ae
amfathy <ahmedeltaify.sw@gmail.com>
2024-12-18 16:56:00 +0200

    update js file with async instead of promise and then

--- API-Json-placeholder/app.js
+++ API-Json-placeholder/app.js
@@ -1,78 +1,73 @@
-function getUsers() {
-    let request = new XMLHttpRequest();
-    request.open("GET", "https://jsonplaceholder.typicode.com/users");
-    request.responseType = "json";
-    request.send();
 
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 300) {
-            let users = request.response;
-            document.getElementById("allUsers").innerHTML ="";
-            if (Array.isArray(users) && users.length > 0) { 
-                users.forEach(user => {
-                    let content = `
-                    <div class="User" onclick="userClicked(${user.id}, this)">
-                        <p class="UserName">${user.name}</p>
-                        <p class="UserEmail">${user.email}</p>
-                    </div>
-                    `;
-                    document.getElementById("allUsers").innerHTML += content;
-                });
-            } else {
-                alert("No users found or invalid data.");
-            }
-        } else {
-            alert("Error fetching users. Status: " + request.status);
-        }
-    };
+async function fetchingUsers(){
+    let response = await fetch("https://jsonplaceholder.typicode.com/users"); 
+    let json = await response.json();
+    return json ; 
+}
 
-    request.onerror = function () {
-        alert("Request failed. Please check your connection.");
-    };
+async function getUsers(){
+    let users = await fetchingUsers();
+    let allUsersContainer = document.getElementById("allUsers");
+    allUsersContainer.innerHTML = "";
+   
+    if (Array.isArray(users) && users.length > 0) { 
+        let content ="";
+        users.forEach(user => {
+             content +=` 
+            <div class="User" onclick="userClicked(${user.id}, this)">
+                <p class="UserName">${user.name}</p>
+                <p class="UserEmail">${user.email}</p>
+            </div>
+            `;
+        })
+        allUsersContainer.innerHTML = content;
+    }
 }
 
 
-function getPosts(userIdd) {
-    let request = new XMLHttpRequest();
-    request.open("GET", "https://jsonplaceholder.typicode.com/posts?userId="+userIdd);
-    request.responseType = "json";
-    request.send();
+async function fetchingPosts(uId){
+    await getUsers();
+    let response =await fetch("https://jsonplaceholder.typicode.com/posts?userId="+uId);
+    let json = await response.json(); 
+    return json  ; 
+}
 
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 300) {
-            let posts = request.response;
-            document.getElementById("mainContent").innerHTML ="";
-            if (Array.isArray(posts) && posts.length > 0) { 
-                posts.forEach(post => {
-                        let content = `
-                        <div class="para">
-                            <p class="paraTitle">${post.title}</p>
-                            <p class="paraBody">${post.body}</p>
-                        </div>
-                        `;
-                        document.getElementById("mainContent").innerHTML += content;
-                });
-            } else {
-                alert("No users found or invalid data.");
-            }
-        } else {
-            alert("Error fetching users. Status: " + request.status);
-        }
-    };
 
-    request.onerror = function () {
-        alert("Request failed. Please check your connection.");
-    };
+async function getPosts(id){
+    userPosts = await fetchingPosts(id) ; 
+    let container = document.getElementById("mainContent");
+    container.innerHTML="";
+    let content ="";
+    userPosts.forEach(post => {
+         content += `
+            <div class="para">
+                <p class="paraTitle">${post.title}</p>
+                <p class="paraBody">${post.body}</p>
+            </div>
+        `;
+    });
+    container.innerHTML = content;
 }
 
 getUsers();
 function userClicked(id , el){
+    
+    
     getPosts(id);
+    
     let selectedElments = document.getElementsByClassName("selected");
-    for(ele of selectedElments){
+    for(let ele of selectedElments){
         ele.classList.remove("selected");
     }
     el.classList.add("selected");
 }
 
- 
+
+
+
+
+
+
+
+
+
